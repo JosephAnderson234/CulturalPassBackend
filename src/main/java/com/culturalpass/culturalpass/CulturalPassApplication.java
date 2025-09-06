e4debddedac29912998dd56b3dd93bbdef5e5d70
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-09-05 20:41:06 -0500

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/culturalpass/culturalpass/CulturalPassApplication.java
@@ -0,0 +1,13 @@
+package com.culturalpass.culturalpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturalPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/culturalpass/culturalpass/CulturalPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturalpass.culturalpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
