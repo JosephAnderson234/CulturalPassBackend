d48f1431f9d951fa0beb25e8ddd5629182d4f414
Yiuseppe24 <83091452+JosephAnderson234@users.noreply.github.com>
2025-10-14 05:37:44 +0000

    fix: Type bug

--- src/main/java/com/culturalpass/culturalpass/User/dto/UserResponseDto.java
+++ src/main/java/com/culturalpass/culturalpass/User/dto/UserResponseDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
-    private String id;
+    private Long id;
     private String email;
     private String firstName;
     private String lastName;
